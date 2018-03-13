d84141abe0d64d076a51ca0d5e5a86cfc6749625
seongyun94 <seongyun2580@gmail.com>
2018-03-13 17:17:48 +0900

    Add ds18b20.ino

---
+++ ds18b20.ino
@@ -0,0 +1,100 @@
+#include <OneWire.h>
+#include <DallasTemperature.h>
+#include <ESP8266WiFi.h>
+
+// Data wire is plugged into port D4 on the Arduino
+#define ONE_WIRE_BUS D4
+
+const char* host = "api.thingspeak.com";
+String ApiKey = "X0H2CVES01LVUESN";
+const char* ssid = "iptime";
+const char* pwd = "12341234";
+
+
+// Setup a oneWire instance to communicate with any OneWire devices (not just Maxim/Dallas temperature ICs)
+OneWire oneWire(ONE_WIRE_BUS);
+
+// Pass our oneWire reference to Dallas Temperature. 
+DallasTemperature sensors(&oneWire);
+
+void connectWiFi()
+{
+  Serial.print("connecting to ");
+  Serial.println(ssid);
+  WiFi.begin(ssid,pwd);
+  while(WiFi.status() != WL_CONNECTED) {
+    delay(1000);
+    Serial.print(".");
+  }
+  Serial.print("\nConnected!, IP address is ");
+  Serial.println(WiFi.localIP());
+}
+
+void sendTemperatureToThingspeak(float temp)
+{
+  WiFiClient client;
+  
+  /*
+  if(!client.connect(host, 80)) {
+    Serial.print("connection failed");
+    Serial.print("\n");
+    return;
+  }
+  */
+  
+  if(client.connect(host, 80)) {
+    Serial.println("WiFi Client connected");
+    String path = ApiKey;
+    path += "&field1=";
+    path += String(temp);
+    path += "\r\n\r\n";
+
+    client.print("POST /update HTTP/1.1\n");
+    client.print("Host: api.thingspeak.com\n");
+    client.print("Connection: close\n");
+    client.print("X-THINGSPEAKAPIKEY: " + ApiKey + "\n");
+    client.print("Content-Type: application/x-www-form-urlencoded\n");
+    client.print("Content-Length: ");
+    client.print(path.length());
+    client.print("\n\n");
+    client.print(path);
+    //delay(1000);
+  }
+  client.stop();
+    //client.println(postStr);
+    //client.println("User-Agent: Seongyun Kim");
+    //client.println("Host: " + String(host));
+    //client.println("Connection: close");
+    //client.println();
+    /*
+    Serial.println(postStr);
+    while(client.connected()) {
+      String line = client.readStringUntil('\n');
+      //Serial.println("what?");
+      Serial.println(line);
+    }
+    */
+}
+void setup(void)
+{
+  // start serial port
+  Serial.begin(115200);
+  Serial.println("Dallas Temperature IC Control Library Demo");
+  connectWiFi();
+  // Start up the library
+  sensors.begin();
+}
+void loop(void)
+{ 
+  // call sensors.requestTemperatures() to issue a global temperature 
+  // request to all devices on the bus
+  Serial.print("Requesting temperatures...");
+  sensors.requestTemperatures(); // Send the command to get temperatures
+  Serial.println("DONE");
+  
+  Serial.print("Temperature for the device 1 (index 0) is: ");
+  Serial.println(sensors.getTempCByIndex(0));  
+
+  sendTemperatureToThingspeak(sensors.getTempCByIndex(0));
+  delay(60000);
+}
